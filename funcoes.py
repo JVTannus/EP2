f59e48c2b8736d387b9a9413368b73d52f4248c7
JVTannus <joaovgbt@al.insper.edu.br>
2025-05-05 17:37:51 -0300

    questão 2

--- funcoes.py
+++ funcoes.py
@@ -4,3 +4,8 @@ def rolar_dados(numero):
     return [random.randint(1,6) for i in range(numero)]
 
 #2
+def guardar_dado(dados_rolados, dados_guardados, indice):
+    dado = dados_rolados[indice]
+    novos_dados_rolados = dados_rolados[:indice] + dados_rolados[indice+1:]
+    novos_dados_guardados = dados_guardados + [dado]
+    return novos_dados_rolados, novos_dados_guardados
