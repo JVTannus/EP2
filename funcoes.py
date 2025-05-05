012f404e65f57bee1324cb3f34ba246b9f687e35
JVTannus <joaovgbt@al.insper.edu.br>
2025-05-05 17:43:44 -0300

    quest 3

--- funcoes.py
+++ funcoes.py
@@ -13,3 +13,10 @@ def guardar_dado(dados_rolados, dados_guardados, indice):
     
     dados_guardados.append(d)
     return [resp, dados_guardados]
+
+#3
+def remover_dado(dados_rolados, dados_no_estoque, dado_para_remover):
+    dado = dados_no_estoque[dado_para_remover]
+    novos_dados_rolados = dados_rolados + [dado]
+    novos_dados_no_estoque = dados_no_estoque[:dado_para_remover] + dados_no_estoque[dado_para_remover+1:]
+    return novos_dados_rolados, novos_dados_no_estoque
