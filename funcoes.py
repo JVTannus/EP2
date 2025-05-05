573af414b08834009b660877c4ce514d935d7294
JVTannus <joaovgbt@al.insper.edu.br>
2025-05-05 17:40:39 -0300

    quest 2 novo

--- funcoes.py
+++ funcoes.py
@@ -5,7 +5,11 @@ def rolar_dados(numero):
 
 #2
 def guardar_dado(dados_rolados, dados_guardados, indice):
-    dado = dados_rolados[indice]
-    novos_dados_rolados = dados_rolados[:indice] + dados_rolados[indice+1:]
-    novos_dados_guardados = dados_guardados + [dado]
-    return novos_dados_rolados, novos_dados_guardados
+    d = dados_rolados[indice]
+    resp = []
+    for i in range(len(dados_rolados)):
+        if i != indice:
+            resp.append(dados_rolados[i])
+    
+    dados_guardados.append(d)
+    return [resp, dados_guardados]
